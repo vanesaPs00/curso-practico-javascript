f90fbbb5558c52f4617a2ffe870b47002d26742f
vanesa pallares <vpallaressolis@gmail.com>
2022-04-10 16:53:23 -0500

    El archivo figura se le agrego funciones

--- figuras.js
+++ figuras.js
@@ -1,86 +1,48 @@
 
 //Código del Cuadrado
 
-//Datos Iniciales del Cuadrado
-console.group("Cuadrados")
-const ladoCuadrado=5;
-console.log("Los lados del cuadrodo miden: "+ladoCuadrado+"cm");
+//Función Perimetro del Cuadrado
 
+function perimetroCuadrado(lado){
+    return lado*4;
+}
 
-//Datos del Perimetro del Cuadrado
-console.group("Perimetro Del Cuadrado")
-const perimetroCuadrado= ladoCuadrado*4;
-console.log("El perimetro del cuadrado es: "+perimetroCuadrado+"cm");
-console.groupEnd();
-
-//Datos del área del Cuadrado
-console.group("Área del Cuadrado")
-const areaCuadrado= ladoCuadrado*ladoCuadrado;
-console.log("El área del cuadrado es: "+areaCuadrado+"cm^2");
-console.groupEnd();
-console.groupEnd();
+//Función área del Cuadrado
+function areaCuadrado(lado) {
+    return lado * lado;
+}
 
 
 //Código del Triángulo
+//Función Perimetro del Triángulo
+function perimetroTriangulo(lado1,lado2,base)
+{
+    return lado1+ lado2+base;
+}
 
-//Datos iniciales del Triángulo
-console.group("Triangulos")
-const ladoTriangulo1=6;
-const ladoTriangulo2=6;
-const baseTriangulo=4;
-const alturaTriangulo=5.5
-console.log("Los lados del triángulo miden: "
-+ladoTriangulo1
-+"cm, "
-+ladoTriangulo2
-+"cm, "
-+baseTriangulo
-+"cm"
-);
-
-//Altura del Triángulo
-console.group("Altura del Triangulo");
-console.log("La altura del triangulo es de: "+alturaTriangulo);
-console.groupEnd();
-
-//Perimetro del Triángulo
-console.group("Perimetro del Triágulo");
-const perimetroTriangulo=ladoTriangulo1+ladoTriangulo2+baseTriangulo;
-console.log("El perimetro del triangulo es: "+perimetroTriangulo);
-console.groupEnd();
-
-//Área del Triángulo
-console.group("Área Triángulo");
-const areTriangulo=(baseTriangulo*alturaTriangulo)/2;
-console.log("El área del Triángulo es: "+areTriangulo);
-console.groupEnd();
-console.groupEnd();
+function areaTriangulo(base,altura)
+{
+    return (base*altura)/2;
+}
 
 //Código del Círculo 
 
-//Datos Iniciales
-console.group("Círculo");
-
-//Radio
-const radioCirculo=4;
-console.log("El radio del circulo es: "+radioCirculo);
-
 //Diámetro
-const diametroCirculo=radioCirculo*2;
-console.log("El diámetro del circulo es: "+diametroCirculo);
-
-
-//pi
-const PI= Math.PI;
-console.log("PI es: "+PI+ "cm");
-
+function diametroCirculo(radio) {
+    return radio*2;
+}
+const PI=Math.PI;
 //Circunferencia
-const perimetroCirculo=diametroCirculo*PI;
-console.log("El diámetro del circulo es: "+perimetroCirculo)
+function perimetroCirculo(radio) {
+    const diametro= diametroCirculo(radio);
+    return diametro * PI;
+}
 
 //Área 
-const areaCirculo=(radioCirculo*radioCirculo)*PI;
-console.log("El area del Circulo es: "+ areaCirculo+"cm²")
+function areaCirculo(radio) {
+    return (radio*radio)*PI;
+}
+
 
 
 
