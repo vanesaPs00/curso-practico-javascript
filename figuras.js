afdcd65f2d78fd5c3e61c70413da79c36cf356d7
vanesa pallares <vpallaressolis@gmail.com>
2022-04-10 19:28:21 -0500

    Se hizo la estructura en html de calcular area y perimetro a un cuadrado y se realizo dos funciones en el archivo figuras.JS

--- figuras.js
+++ figuras.js
@@ -1,3 +1,9 @@
 
-
-console.log("vanesa")
+//Aquí interactuamos en el HTML
+function calcularPerimetroCuadrado(){
+    const input=document.getElementById("inputCuadrado");
+    const value=input.value;
+}
+function calcularAreaCuadrado(){
+    
+}
